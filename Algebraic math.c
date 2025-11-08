7cb1961f5a287d00179c23b798adc1199c839420
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-11-08 08:07:10 +0600

    Update and rename 5th Code to Algebraic math.c

--- Algebraic math.c
+++ Algebraic math.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
-
 int main ()
-
 {
 
 float a;
-
 float b;
 
 printf("Enter your 1st valu: ");
-
 scanf ("%f",&a);
 
 printf ("Enter your 2nd valu :");
-
 scanf ("%f",&b);
 
 float avarage = (a+b)/2;
-
 printf("avarage is %f", (a+b)/2);
 
 return 0;
-
 }
